e91d57430f3b11331c28d2246d6cff9a303d73d2
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-07-11 20:33:42 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,13 @@
+def fibo(n):
+    if n == 0:
+        return 0
+    
+    elif n <= 2:
+        return 1
+    
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+    
+
+n = int(input())
+print(fibo(n))
