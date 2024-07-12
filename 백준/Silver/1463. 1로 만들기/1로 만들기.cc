0d33cc5d37adeda356a84714676e93fbb81cd87d
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-07-12 15:49:06 +0900

    [Silver III] Title: 1로 만들기, Time: 36 ms, Memory: 37056 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#define MAX 1000001
+using namespace std;
+
+int memo[MAX];
+
+int getMinCalc(int n) {
+    if (n == 1) return 0;
+    if (memo[n] != -1) return memo[n];
+
+    int result = getMinCalc(n - 1) + 1;
+    if (n % 2 == 0)
+        result = min(result, getMinCalc(n / 2) + 1);
+    if (n % 3 == 0)
+        result = min(result, getMinCalc(n / 3) + 1);
+    
+    memo[n] = result;
+    return result;
+}
+
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    fill(memo, memo+MAX, -1);
+    printf("%d\n", getMinCalc(n));
+    return 0;
+}
