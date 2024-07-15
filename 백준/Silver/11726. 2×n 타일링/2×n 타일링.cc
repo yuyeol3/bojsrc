f2da52a1e5e4931bc552191612831a7c0f5836ec
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-07-15 15:33:18 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -2,19 +2,18 @@
 #include <vector>
 #define DIV 10007
 
+
 int f(int n) {
     if (n == 1) return 1;
     if (n == 2) return 2;
-
-    int prev, pprev;
-    prev = 2;
-    pprev = 1;
+    
+    std::vector<int> dp(n+1, -1);
+    dp[1] = 1;
+    dp[2] = 2;
     for (int i = 2; i < n; ++i) {
-        int temp = prev;
-        prev = ((prev%DIV)+(pprev%DIV))%DIV;
-        pprev = temp;
+        dp[i+1] = ((dp[i]%DIV)+(dp[i-1]%DIV))%DIV;
     }
-    return prev;
+    return dp[n];
 }
 
 int main() { 
