141769c897fb5b5c0113b3abc6a0bcbc44ef8bd0
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-07-20 12:35:24 +0900

    [Silver II] Title: 제곱수의 합, Time: 32 ms, Memory: 1620 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -1,49 +1,22 @@
 #include <cstdio>
 #include <vector>
 #include <cmath>
-#define FAIL __INT_MAX__ -1
-#define RMAX 100001
-#define CMAX 317
+#include <algorithm>
+#include <climits>
 using namespace std;
 
-int dp[RMAX][CMAX];
-
-inline int sq(int x) {
-    return x*x;
-}
-
-int f(int k, int num) {
-    if (k == 0)                        return 0;
-    if (k == sq(num))                  return dp[k][num] = 1;
-    if (k < 0 || k < num)              return FAIL;
-    if (k < sq(num))                   return FAIL;
-    if (num < 0)                       return FAIL;
-    if (dp[k][num] == FAIL)            return FAIL;
-    if (dp[k][num] != __INT_MAX__)     return dp[k][num];
-
-    int res = FAIL;
-    res = min(res, 0 + f( k, num+1 ));
-    if (k-sq(num) >= 0) {
-        // res = min(res, 1 + f( k-sq(num), num+1 ));
-        res = min(res, 1 + f( k-sq(num), num ));
-    } 
-
-
-
-    dp[k][num] = res >= FAIL ? FAIL : res;
-    return dp[k][num];
-}
-
-
-int main(int argc, char **argv) {
+int main() {
     int N;
     scanf("%d", &N);
-    // sscanf(argv[1], "%d", &N);
-    for (int i = 0; i < RMAX; ++i) {
-        for (int j = 0; j < CMAX; ++j) {
-            dp[i][j] = __INT_MAX__;
+    vector<int> dp(N+1, INT_MAX);
+    dp[0] = 0;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j*j <= i; j++) {
+            dp[i] = min(dp[i], dp[i - j*j] + 1);
         }
     }
-    printf("%d\n", f(N, 1));
+
+    printf("%d\n", dp[N]);
     return 0;
 }
