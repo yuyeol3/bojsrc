f94dbb6bea576b620ea90ab5e6172c4e913f74d0
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-07-24 13:20:37 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 1228 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -3,15 +3,14 @@
 #define DIV 1000000000
 using namespace std;
 
-vector<vector<long long>> dp;
-
 int main() {
     int N;
     scanf("%d", &N);
-    dp.resize(N+1, vector<long long>(10,0));
-    
-    fill(dp[1].begin()+1,dp[1].end(),1);
+    vector<vector<long long>> dp(N+1, vector<long long>(10,0));
 
+    // N = 1인 경우 0을 제외한 1~9까지 9개 사용가능
+    fill(dp[1].begin()+1,dp[1].end(),1);
+    // dp로 풀기
     for (int i = 2; i <= N; ++i) {
         for (int j = 0; j <= 9; ++j) {
             if (j == 0) {
@@ -25,7 +24,6 @@ int main() {
                 dp[i][j] += dp[i-1][j+1] % DIV;
                 dp[i][j] %= DIV;
             }
-            
         }
     }
 
@@ -33,7 +31,6 @@ int main() {
     for (int i = 0; i < 10; ++i) {
         res += dp[N][i] % DIV;
     }
-
-    printf("%d\n", res % DIV);
+    printf("%lld\n", res % DIV);
     return 0;
 }
