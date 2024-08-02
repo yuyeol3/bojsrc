4fd0f29d759b8692e136a29ae045d745eaadf0a1
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-08-02 13:53:26 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 8 ms, Memory: 5028 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -4,33 +4,26 @@
 using namespace std;
 
 int dp[MAX][MAX];
-vector<int> series;
-
+int series[MAX];
+int N;
 /*
  * idx부터 가장 큰 증가 수열의 합을 구하는 함수
  */
 int f(int idx, int prev) {
     // printf("%d %d\n", idx, prev);
-    if (idx >= series.size()) return prev;
+    if (idx >= N) return prev;
     if (dp[idx][prev] != -1) return dp[idx][prev];
 
     int res = 0;
     res = max(res, f(idx+1, prev));
     if (series[idx] > prev) res = max(res, prev + f(idx+1, series[idx]));
-    // for (int i = idx; i < series.size(); ++i) {
-    //     if (series[i] > prev) {
-    //         res = max(res, prev + f(i+1, series[i]));
-    //     }
-    // }
 
     dp[idx][prev] = res;
     return res;
 }
 
 int main() {
-    int N;
     scanf("%d", &N);
-    series.resize(N);
 
     int max = 0;
     for (int i = 0; i < N; ++i) {
