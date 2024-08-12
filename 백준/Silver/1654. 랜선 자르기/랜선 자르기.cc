30be89960a54cc4ba45d0b4493e3f73d73670768
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-08-12 12:41:21 +0900

    [Silver II] Title: 랜선 자르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+int main() {
+    int K, N;
+    scanf("%d %d", &K, &N);
+    int length[K];
+    for (int i = 0; i < K; ++i) {
+        scanf("%d", length+i);
+    }
+
+    unsigned int ub, lb;
+    ub = (unsigned int)__INT_MAX__ + 1;
+    lb = 1;
+
+    while (ub > lb) {
+        int mid = (ub+lb)/2;
+
+        if (lb == mid) {
+            printf("%d\n", lb);
+            break;
+        }
+
+        int cableCount = 0;
+        for (int i = 0; i < K; ++i) {
+            cableCount += length[i] / mid;
+        } 
+
+        if (cableCount >= N) {
+            lb = mid;
+        } else {
+            ub = mid;
+        }
+    }
+
+    return 0;
+}
