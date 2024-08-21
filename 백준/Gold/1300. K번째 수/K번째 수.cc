385c5f969a08324f8535ff6b317814bf684fe3e9
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-08-21 20:44:41 +0900

    [Gold I] Title: K번째 수, Time: 24 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.cc
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -7,28 +7,25 @@ int main() {
     long long N, K;
     scanf("%lld", &N);
     scanf("%lld", &K);
-
     long long ub, lb;
     ub = N*N + 1;
     lb = 0;
-
+    
     for (long long mid=(ub+lb)/2;ub>lb;mid=(ub+lb)/2) {
         if (lb == mid) {
             printf("%lld\n", ub);
             break;
         }
-
+        
         long long idx = 0;
-        for (long long i = 1; i <= N; ++i) {
+        for (long long i = 1; i <= N && idx < K; ++i) {
             idx += min(mid / i, N);
         }
-
         if (idx >= K) {
             ub = mid;
         } else {
             lb = mid;
-        }
-        
+        }  
     }
     return 0;
 }
