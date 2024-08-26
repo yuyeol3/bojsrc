2cfe32923ae64f550146a035a5f7d66d1a8db4dc
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-08-26 18:19:28 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 64 ms, Memory: 1304 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <list>
+using namespace std;
+
+inline void iterIncrement(list<int> &p, list<int>::iterator &iter) 
+{
+    if (iter == p.end() || iter == --p.end())  iter = p.begin();
+    else ++iter;
+}
+
+int main() {
+    int N, K;
+    scanf("%d %d", &N, &K);
+
+    list<int> p;
+    for (int i = 1; i <= N; ++i) {
+        p.push_back(i);
+    }
+
+    list<int>::iterator iter = p.begin();
+    printf("<");
+    for (int i = 0; i < N; ++i) {
+
+        for (int j = 0; j < K-1; ++j)
+            iterIncrement(p, iter);
+
+        const char *format = (i == N-1 ? "%d>\n" : "%d, ");
+        printf(format, *iter);
+        list<int>::iterator toDelete = iter;
+        iterIncrement(p, iter);
+        p.erase(toDelete);
+    }
+
+    // 남은 원소 출력
+    // iter = p.begin();
+    // for (int i = 0; i < K - 1; ++i) {
+    //     const char *format = (i == K - 2 ? "%d>\n" : "%d, ");
+    //     printf(format, *(iter++));
+    // }
+    return 0;
+}
