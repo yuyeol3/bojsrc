3e99e2f6cf894c44cb63695ad0d2df93f92b889a
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-10-01 00:41:57 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 472 ms, Memory: 25916 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -1,44 +1,81 @@
 import java.util.Scanner;
 
+class Characters {
+    // 변경할 일이 없으므로 final로..
+    private final char character;
+    private final int num;
+    public Characters(char character, int num) {
+        this.character = character;
+        this.num = num;
+    }
+
+    public void Print() {
+        for (int i = 0; i < num; ++i) {
+            System.out.print(character);
+        }
+    }
+}
+
+class Line {
+    private final Characters[] characters;
+    private final boolean endline;
+
+    public Line (Characters ... chars) {
+        characters = chars;
+        this.endline = true;
+    }
+
+    public Line (boolean endline, Characters ... chars) {
+        characters = chars;
+        this.endline = endline;
+    }
+
+    public void Print() {
+        for (Characters c : characters) {
+            c.Print();
+        }
+        
+        if (endline)
+            System.out.println();
+            
+    }
+}
+
+class Pattern {
+    private final Line[] lines;
+
+    public Pattern(int n) {
+        // 필요한 줄 개수만큼 인스턴스 생성
+        lines = new Line[2*n-1];
+        for (int i = 0; i < n; ++i) {
+            int starNum = (i+1);
+            int blankNum = 2*(n - starNum);
+            lines[i] = new Line(
+                new Characters('*', starNum),    
+                new Characters(' ', blankNum),
+                new Characters('*', starNum) 
+            );
+        }
+
+        for (int i = 0; i < n; ++i) {
+            lines[2*n - 2 - i] = lines[i]; 
+        }
+        
+
+    }
+
+    public void Print() {
+        for (Line line: lines) {
+            line.Print();
+        }
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for (int i = 0; i < n-1; ++i) {
-            int starNum = i + 1;
-            int blankNum = n * 2 - ((i+1) * 2);
-            for (int j = 0; j < starNum; ++j) {
-                System.out.printf("*");
-            }
-
-            for (int j = 0; j < blankNum; ++j) {
-                System.out.printf(" ");
-            }
-
-            for (int j = 0; j < starNum; ++j) {
-                System.out.printf("*");
-            }
-            System.out.printf("\n");
-        }
-
-
-        for (int i = n - 1; i >= 0; --i) {
-            int starNum = i + 1;
-            int blankNum = n * 2 - ((i+1) * 2);
-            for (int j = 0; j < starNum; ++j) {
-                System.out.printf("*");
-            }
-
-            for (int j = 0; j < blankNum; ++j) {
-                System.out.printf(" ");
-            }
-
-            for (int j = 0; j < starNum; ++j) {
-                System.out.printf("*");
-            }
-            System.out.printf("\n");
-        }
-
+        new Pattern(sc.nextInt()).Print();
     }
+
 }
