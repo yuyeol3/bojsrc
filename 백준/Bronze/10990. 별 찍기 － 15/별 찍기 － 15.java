6ea3c5b3f5aaad399eb2534963aba7a2c0396032
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-10-10 20:49:54 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 356 ms, Memory: 22856 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.java
@@ -0,0 +1,115 @@
+import java.util.*;
+
+interface LineFactory {
+    int lineNum();
+    Line create(int i);
+}
+
+interface Content {
+    void print();
+}
+
+class Characters implements Content {
+    private final char character;
+    private final int num;
+    public Characters(char character, int num) {
+        this.character = character;
+        this.num = num;
+    }
+
+    public void print() {
+        for (int i = 0; i < num; ++i) {
+            System.out.print(character);
+        }
+    }
+}
+
+
+class CharactersWithStars implements Content {
+    private final Content content;
+
+    public CharactersWithStars(Content content) {
+        this.content = content;
+    }
+
+    public void print() {
+        System.out.print('*');
+        content.print();
+        System.out.print('*');
+    }
+
+}
+
+
+class Line {
+    private final List<Content> charactersList;
+    public Line(List<Content> list) {
+        charactersList = list;
+    }
+
+    public void print() {
+        for (Content characters : charactersList) {
+            characters.print();
+        }
+    }
+
+}
+
+class Pattern {
+    private final List<Line> linesList;
+    public Pattern(LineFactory lineFactory) {
+        linesList = new ArrayList<>(lineFactory.lineNum());
+        for (int i = 0; i < lineFactory.lineNum(); ++i) {
+            linesList.add(lineFactory.create(i));
+        }
+    }
+
+    public void print() {
+        for (Line line : linesList) {
+            line.print();
+            System.out.println();
+        }
+    }
+}
+
+class P10990 implements LineFactory {
+    private final int num;
+    public P10990(int n) {
+        num = n;
+    }
+
+    public int lineNum() {
+        return num;
+    }
+
+    public Line create(int i) {
+        List<Content> list = new ArrayList<>();
+        int totWidth = num*2 - 1;
+
+        if (i == 0) {
+            list.add(new Characters(' ', (totWidth - 1)/2 ));
+            list.add(new Characters('*', 1));
+        }
+        else {
+            int innerBlankNum = (i+1)*2 - 3;
+            int outerBlankNum = (totWidth - innerBlankNum - 2) / 2;
+            list.add(new Characters(' ', outerBlankNum));
+            list.add(new CharactersWithStars(new Characters(' ', innerBlankNum) ) );
+        }
+
+        return new Line(list);
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        new Pattern(new P10990(sc.nextInt())).print();
+        sc.close();
+    }
+}
+
+
+
+
