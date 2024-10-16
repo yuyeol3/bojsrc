6a05a5774135cce3b53ad18ca6935a669ff63130
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-10-16 19:46:45 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 114.79 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Stack {
+public:
+    Stack(int max): top(-1) {
+        arr = new int[max];
+    }
+    
+    
+    void Pop() {
+        top--;
+    }
+    
+    int Top() {
+        return arr[top];
+    }
+    
+    void Push(int item) {
+        arr[++top] = item;
+    }
+    
+    ~Stack() {
+        delete []arr;
+    }
+    
+private:
+    int *arr;
+    int top;
+};
+
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    Stack st(arr.size());
+    
+    st.Push(arr[0]);
+    answer.push_back(arr[0]);
+    for (int i = 1; i < arr.size(); ++i) {
+        if (st.Top() != arr[i])
+            answer.push_back(arr[i]);
+        
+        st.Push(arr[i]);
+    }
+    
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    // cout << "Hello Cpp" << endl;
+
+    return answer;
+}
