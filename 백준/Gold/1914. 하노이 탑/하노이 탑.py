a52394f1f13552bc9d4126461fe571643123d96e
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-10-18 21:33:32 +0900

    [Gold V] Title: 하노이 탑, Time: 44 ms, Memory: 48964 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -6,10 +6,14 @@ def hanoi(n, start, end, via):
     if (n == 1):
         return f"{start} {end}\n"
     
+    if (f"{n},{start},{end},{via}" in dp):
+        return dp[f"{n},{start},{end},{via}"]
+    
     res = ""
     res += hanoi(n-1, start, via, end)
     res += f"{start} {end}\n"
     res += hanoi(n-1, via, end, start)
+    dp[f"{n},{start},{end},{via}"] = res
     return res
 
 
