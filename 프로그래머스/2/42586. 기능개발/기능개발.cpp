d60185a36fa532bf37222f462483eb858bb2bacc
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-10-22 00:31:27 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Queue {
+public:
+    Queue(int max)
+    {
+        maxSize = max+1;
+        front = maxSize - 1;
+        rear = maxSize - 1;
+        info = new int[maxSize];
+    }
+    
+    Queue(vector<int> &v) {
+        maxSize = v.size() + 1;
+        front = maxSize - 1;
+        rear = maxSize - 1;
+        info = new int[maxSize];
+        
+        for (int e : v) {
+            Enqueue(e);
+        }
+    }
+    
+    
+    void Enqueue(int item) {
+        rear = (rear + 1) % maxSize;
+        info[rear] = item;
+    }
+    
+    int Dequeue() {
+        front = (front + 1) % maxSize;
+        return info[front];
+    }
+    
+    bool IsFull() {
+        return ((rear + 1) % maxSize == front);
+    }
+    
+    bool IsEmpty() {
+        return rear == front;
+    }
+    
+    ~Queue() {
+        delete []info;
+    }
+    
+private:
+    int *info;
+    int front, rear;
+    int maxSize;
+};
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer(progresses.size(), 0);
+    Queue workQueue(progresses);
+    
+    int location = -1;
+    int workPeriod = 0;    
+    for (int i = 0; i < progresses.size(); ++i) {
+        
+        int targetWorkProgress = workQueue.Dequeue();
+        
+        if (targetWorkProgress + workPeriod*speeds[i] >= 100) {
+            answer[location]++;
+        }
+        else {
+            workPeriod = (100 - targetWorkProgress)/speeds[i];
+            workPeriod += ((100 - progresses[i])%speeds[i] > 0 ? 1 : 0);
+            answer[++location]++;
+        }
+    }
+    
+    answer.resize(location+1);
+    // answer.shrink_to_fit();
+    return answer;
+}
