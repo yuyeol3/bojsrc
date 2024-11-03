313bdb420c585e7cbeceb385938032631a93e14c
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-11-04 08:46:08 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        sliced = array[command[0]-1:command[1]]
+        sliced.sort()
+        answer.append(sliced[command[2]-1])
+        
+    
+    return answer
