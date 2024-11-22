f446e2ecdac00074eb94e772644a4e29f0a659f7
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-11-22 22:41:24 +0900

    [level 2] Title: 타겟 넘버, Time: 441.83 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,26 +1,9 @@
-
-
-def dfs(numbers, operations, target, idx=0, static_num=[0]):
-    evaluated = 0
-    for i, val in enumerate(numbers):
-        evaluated += val * operations[i]
+def dfs(numbers, target, index=0, current_sum=0):
+    if index == len(numbers):
+        return 1 if current_sum == target else 0
     
-    if (evaluated == target):
-        static_num[0] += 1
-        return
-    
-    elif (idx >= len(numbers)):
-        return
-    
-    for i in range(idx, len(operations)):
-        new_op = operations[:]
-        new_op[i] = -1
-        dfs(numbers, new_op, target, i+1)
-            
-    return static_num[0]
-    
+    return dfs(numbers, target, index + 1, current_sum + numbers[index]) + \
+           dfs(numbers, target, index + 1, current_sum - numbers[index])
 
 def solution(numbers, target):
-    answer = dfs(numbers, [1] * len(numbers), target)
-    
-    return answer
+    return dfs(numbers,target)
