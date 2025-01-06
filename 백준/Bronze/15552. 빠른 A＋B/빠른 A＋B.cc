7ff6f59d2f2555d61066bd20686bef99e77615d3
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2025-01-06 21:07:12 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()  
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int N, a, b;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+
+    return 0;
+}
