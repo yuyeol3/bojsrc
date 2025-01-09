075664edb4d251d4abc57d0e188771cba3a3264b
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2025-01-09 22:22:55 +0900

    [Silver II] Title: 에디터, Time: 32 ms, Memory: 11088 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,48 +1,92 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAX = 1000000;
+char dat[MAX];
+int prv[MAX], nxt[MAX];
+int usable = 1;
+int len = 0;
+
+void traverse() {
+    int cur = nxt[0];
+    while (cur != -1) {
+        cout << dat[cur];
+        cur = nxt[cur];
+    }
+    cout << '\n';
+}
+
+void rtraverse() {
+    int cur = usable - 1;
+    while (cur != 0) {
+        cout << dat[cur];
+        cur = prv[cur];
+    }
+    cout << '\n';
+}
+
+
+void add(int addr, char data) {
+    dat[usable] = data;
+    prv[usable] = addr;
+    nxt[usable] = nxt[addr];
+
+    if (nxt[addr] != -1) prv[nxt[addr]] = usable;
+    nxt[addr] = usable;
+    usable++;
+    len++;
+}
+
+void erase(int addr) {
+    if (nxt[addr]!= -1) prv[nxt[addr]] = prv[addr];
+    nxt[prv[addr]] = nxt[addr];
+    len--;
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    list<char> editor;
-    list<char>::iterator cursor;
+    fill(prv, prv+MAX, -1);
+    fill(nxt, nxt+MAX, -1);
 
     string ln;
     cin >> ln;
-    for (char c : ln) editor.push_front(c);
 
-    cursor = editor.begin();
+    for (char c : ln) add(len, c);
+
+    int cursor = len; // 제일 마지막 단어
+
+    // traverse();
 
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++) 
-    {
+    for (int i = 0; i < n; i++) {
         char command;
         cin >> command;
 
         switch (command) {
             case 'L':
-                if (cursor != editor.end()) cursor++;
+                if (cursor != 0) cursor = prv[cursor];
                 break;
             case 'D':
-                if (cursor != editor.begin()) cursor--;
+                if (nxt[cursor] != -1) cursor = nxt[cursor];
                 break;
             case 'B':
-                if (cursor != editor.end()) cursor = editor.erase(cursor);
+                if (cursor != 0) {
+                    erase(cursor);
+                    cursor = prv[cursor];
+                }
                 break;
             case 'P':
                 cin >> command;
-                cursor = editor.insert(cursor, command);
+                add(cursor, command);
+                cursor = usable - 1;
                 break;
         }
     }
 
-    for (auto iter = editor.rbegin(); iter != editor.rend(); iter++)
-        cout << *iter;
-    // cout << editor.front();
-    cout <<'\n';
-
+    traverse();
     return 0;
 }
