f04a721d9f82a5710b1e60a4e40640671d6095b7
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2025-01-17 16:55:14 +0900

    [Silver IV] Title: 덱, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.cc
+++ 백준/Silver/10866. 덱/덱.cc
@@ -1,11 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAX = 10005;
+int dat[MAX*2+1];
+int head = MAX, tail = MAX;
+
+void push_front(int x) {
+    dat[--head] = x;
+}
+
+void push_back(int x) {
+    dat[tail++] = x;
+}
+
+void pop_front() {
+    head++;
+}
+
+void pop_back() {
+    tail--;
+}
+
+int front() {
+    return dat[head];
+}
+
+int back() {
+    return dat[tail - 1];
+}
+
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    deque<int> dq;
-
     int N;
     cin >> N;
 
@@ -15,39 +42,39 @@ int main() {
         if (command == "push_front") {
             int t;
             cin >> t;
-            dq.push_front(t);
+            push_front(t);
         }
         else if (command == "push_back") {
             int t;
             cin >> t;
-            dq.push_back(t);
+            push_back(t);
         }
         else if (command == "pop_front") {
-            if (!dq.empty()) {
-                cout << dq.front() << '\n';
-                dq.pop_front();
+            if (tail-head > 0) {
+                cout << front() << '\n';
+                pop_front();
             }
             else cout << -1 << '\n';
         }
         else if (command == "pop_back") {
-            if (!dq.empty()) {
-                cout << dq.back() << '\n';
-                dq.pop_back();
+            if (tail-head > 0) {
+                cout << back() << '\n';
+                pop_back();
             }
             else cout << -1 << '\n';
         }
         else if (command == "size") {
-            cout << dq.size() << '\n';
+            cout << tail-head << '\n';
         }
         else if (command == "empty") {
-            cout << dq.empty() << '\n';
+            cout << !(tail-head) << '\n';
         }
         else if (command == "front") {
-            if (!dq.empty()) cout << dq.front() << '\n';
+            if (tail-head>0) cout << front() << '\n';
             else cout << -1 << '\n';
         }
         else if (command == "back") {
-            if (!dq.empty()) cout << dq.back() << '\n';
+            if (tail-head>0) cout << back() << '\n';
             else cout << -1 << '\n';
         }
     }
