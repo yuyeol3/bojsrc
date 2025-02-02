08d60fc074e290f2fa43deb48ca1526189a35e51
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2025-02-02 18:08:35 +0900

    [Silver I] Title: 나이트의 이동, Time: 20 ms, Memory: 2380 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -10,7 +10,8 @@ int dy[] = {-2, -1, +2, +1, -2, -1, +2, +1};
 
 
 int main() {
-
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     int T;
     cin >> T;
     for (int i = 0; i < T; i++) {
