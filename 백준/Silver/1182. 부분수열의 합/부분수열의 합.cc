8c40c035a89b8feedf5af740846657872ea86315
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2025-02-11 22:16:40 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,27 @@
+// 부분집합의 개수 2^N
+// 진부분집합의 개수 2^N - 1 (공집합 제외)
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, S, cnt;
+int dat[20];
+int use[20];
+
+void f(int idx, int sum) {
+    if (idx == N) {
+        cnt += (sum == S ? 1 : 0);
+        return;
+    }
+
+    f(idx+1, sum + dat[idx]);
+    f(idx+1, sum);
+}
+
+int main() {
+    cin >> N >> S;
+    for (int i = 0; i < N; i++) {
+        cin >> dat[i];
+    }
+    f(0, 0);
+    cout << (S == 0 ? cnt - 1 : cnt) << '\n';
+}
