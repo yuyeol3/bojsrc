a23642f1fe5c18b26ef3d13ff6152a15f10bd357
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2025-07-06 13:39:06 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,29 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[305] = {0};
-int arr[305];
-int N;
-
-// n번째를 반드시 포함하는 최대 점수
-int f(int n) {
-    if (n < 0) return 0;
-    if (dp[n]) return dp[n];
-
-    dp[n]= max(arr[n]+arr[n-1]+f(n-3), arr[n]+f(n-2));
-    return dp[n];
-}
+int s[301];
+int dp[301];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> arr[i];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
     }
-    dp[0] = arr[0];
-    cout << f(N-1) << '\n';
 
+    dp[0] = s[0];
+    dp[1] = s[0] + s[1];
+    dp[2] = max(s[0], s[1]) + s[2];
+
+    for (int i = 3; i < n; i++) {
+        dp[i] = max(s[i]+s[i-1]+dp[i-3], s[i]+dp[i-2]);
+    }
+
+    cout << dp[n-1] << '\n';
     return 0;
 }
