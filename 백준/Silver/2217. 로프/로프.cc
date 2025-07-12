159bb2879cff34f187892dc6fd47becae44d6981
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2025-07-12 12:13:43 +0900

    [Silver IV] Title: 로프, Time: 36 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int r[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> r[i];
+
+    sort(r, r+n, [](int a, int b)-> bool {
+        return a > b;
+    });
+
+    // for (auto e : r) cout << e << '\n';
+    
+    int maxi = 0;
+    for (int i = 0; i < n; i++) {
+        maxi = max((i+1) * r[i], maxi);
+    }
+    cout << maxi << '\n';
+}
