1c7ae27a5f95edcc518152d83cf0e9552ca20c11
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2025-07-15 10:36:59 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    int t[n];
+
+    for (int i = 0; i < n; i++) 
+        cin >> t[i];
+
+    sort(t, t+n);
+
+    int prev = 0;
+    int result = 0;
+    for (int e : t) {
+        prev += e;
+        result += prev;
+    }
+    cout << result;
+}
