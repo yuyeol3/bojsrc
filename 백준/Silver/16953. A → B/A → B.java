533ca7123fc36e158cc047ae4aa2c7b8979adcc3
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2026-01-04 13:23:34 +0900

    [Silver II] Title: A → B, Time: 112 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -5,33 +5,33 @@ class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static Map<Long, Integer> visited = new HashMap<>();
     public static void main(String[] args) throws IOException {
         StringTokenizer s = new StringTokenizer(br.readLine());
         long a = Long.parseLong(s.nextToken());
         long b = Long.parseLong(s.nextToken());
 
-        Deque<Long> queue = new ArrayDeque<>();
-        queue.addLast(a);
-        visited.put(a, 0);
+        int cnt = 0;
+        long cur = b;
 
-        while (!queue.isEmpty()) {
-            long node = queue.pop();
-            List<Long> adjs = List.of(node * 2, node * 10 + 1);
-            for (long adj : adjs) {
-                if (visited.get(adj) != null && 
-                    visited.get(node) + 1 >= visited.get(adj)) 
-                    continue;
-                
-                if (adj > b) continue;
+        while (true) {
+            if (cur == a) 
+                break;
 
-                visited.put(adj, visited.get(node) + 1);
-                queue.addLast(adj);
+            if (cur % 2 == 0 && cur / 2 >= a) {
+                cur /= 2;
             }
+            else if (cur % 10 == 1 && cur / 10 >= a) {
+                cur /= 10;
+            }
+            else {
+                bw.write(-1 + "\n");
+                bw.flush();
+                return;
+            }
+            cnt++;
         }
-
-        visited.computeIfAbsent(b, k -> -2);
-        bw.write((visited.get(b) + 1) + "\n");
+        
+        bw.write((cnt+1) + "\n");
         bw.flush();
     }
 }
