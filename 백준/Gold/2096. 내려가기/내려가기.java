e12c11e2f7d184f57f46c5cd1fe3654e16a614fb
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2026-01-06 12:57:53 +0900

    [Gold V] Title: 내려가기, Time: 336 ms, Memory: 42224 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -6,31 +6,37 @@ class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N+5][3];
         int[][][] dp = new int[2][3][2];
         StringTokenizer st;
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+        // for (int i = 0; i < N; i++) {
+        //     st = new StringTokenizer(br.readLine());
+        //     for (int j = 0; j < 3; j++) {
+        //         arr[i][j] = Integer.parseInt(st.nextToken());
+        //     }
+        // }
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 3; i++) {
-            dp[0][i][0] = arr[0][i];
-            dp[0][i][1] = arr[0][i];
+            int e = Integer.parseInt(st.nextToken());
+            dp[0][i][0] = e;
+            dp[0][i][1] = e;
         }
 
         int prev = 0, next = 1;
         for (int i = 1; i < N; i++) {
-            dp[next][0][0] = Math.min(dp[prev][0][0], dp[prev][1][0]) + arr[i][0];
-            dp[next][0][1] = Math.max(dp[prev][0][1], dp[prev][1][1]) + arr[i][0];
+            int one, two, three;
+            st = new StringTokenizer(br.readLine());
+            one = Integer.parseInt(st.nextToken());
+            two = Integer.parseInt(st.nextToken());
+            three = Integer.parseInt(st.nextToken());
+            dp[next][0][0] = Math.min(dp[prev][0][0], dp[prev][1][0]) + one;
+            dp[next][0][1] = Math.max(dp[prev][0][1], dp[prev][1][1]) + one;
 
-            dp[next][1][0] = Math.min(Math.min(dp[prev][0][0], dp[prev][1][0]), dp[prev][2][0]) +  arr[i][1];
-            dp[next][1][1] = Math.max(Math.max(dp[prev][0][1], dp[prev][1][1]), dp[prev][2][1]) +  arr[i][1];
+            dp[next][1][0] = Math.min(Math.min(dp[prev][0][0], dp[prev][1][0]), dp[prev][2][0]) + two;
+            dp[next][1][1] = Math.max(Math.max(dp[prev][0][1], dp[prev][1][1]), dp[prev][2][1]) + two;
 
-            dp[next][2][0] = Math.min(dp[prev][1][0], dp[prev][2][0]) + arr[i][2];
-            dp[next][2][1] = Math.max(dp[prev][1][1], dp[prev][2][1]) + arr[i][2];
+            dp[next][2][0] = Math.min(dp[prev][1][0], dp[prev][2][0]) + three;
+            dp[next][2][1] = Math.max(dp[prev][1][1], dp[prev][2][1]) + three;
 
             prev = (prev + 1) % 2;
             next = (next + 1) % 2;
