ce7dfda16a376cc3123a646eeacd4c2728bcf78d
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2026-01-24 12:52:06 +0900

    [Silver III] Title: 프린터 큐, Time: 112 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -14,34 +14,45 @@ class Main {
             this.idx = idx;
         }
     }
+
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        
+
         while (T-- >0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N, M;
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             
-            PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)->Integer.compare(b, a));
+            int[] priorities = new int[11];
             Deque<Pair> dq = new ArrayDeque<>();
             st = new StringTokenizer(br.readLine());
 
+            // O(N)
+            int pIdx = 0;
             for (int i = 0; i < N; i++) {
                 int t = Integer.parseInt(st.nextToken());
-                pq.offer(t);
+                priorities[t]++;
+                if (t > pIdx) pIdx = t;
                 dq.addLast(new Pair(t, i));
             }
 
+            // O(N^2)
             int idx = 1;
+
             while (!dq.isEmpty()) {
                 Pair e = dq.pollFirst();
-                if (pq.peek() > e.num) {
+                if (priorities[pIdx] >= 1 && pIdx > e.num) {
                     dq.addLast(e);
                     continue;
                 }
 
-                pq.poll();
+                priorities[pIdx]--;
+                while (priorities[pIdx] == 0 && pIdx > 0)
+                    pIdx--;
+
                 if (e.idx == M) {
                     sb.append(idx).append("\n");
                     break;
