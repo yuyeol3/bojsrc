a1ccc4bf87871c83331ce98d71532f1c8cfd0d0b
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2026-01-29 17:21:01 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 184 ms, Memory: 20352 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,57 +1,52 @@
 import java.io.*;
+import java.util.*;
 
 class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int MAX_NODE = 10005;
     static StringBuilder sb = new StringBuilder();
-    static int NODE_MAX = 10005;
-    static int[] node = new int[NODE_MAX];
-    static int[] left = new int[NODE_MAX];
-    static int[] right = new int[NODE_MAX];
-    static int root = 1;
-    static int free_node = 1;
-    
-    static void addTree(int x) {
-        if (root == free_node) {
-            node[free_node++] = x;
-            return;
-        }
-        int next = root;
-        int prev = 0;
-        while (true) {
-            prev = next;
-            if (node[next] < x) {
-                next = right[next];
-                if (next == 0) {
-                    node[free_node++] = x;
-                    right[prev] = free_node-1;
-                    return;
-                }
-            }
-            else if (node[next] > x) {
-                next = left[next];
-                if (next == 0) {
-                    node[free_node++] = x;
-                    left[prev] = free_node-1;
-                    return;
-                }
-            }
-        }
-    }
-
-    static void postOrder(int x) {
-        if (x == 0) return;
-        postOrder(left[x]);
-        postOrder(right[x]);
-        sb.append(node[x]).append("\n");
+    static class Node {
+        int left = 0;
+        int right = 0;
     }
 
     public static void main(String[] args) throws IOException {
+
+        Map<Integer, Node> tree = new HashMap<>();
+        Deque<Integer> stk = new ArrayDeque<>();
+
+        int root = Integer.parseInt(br.readLine());
+        tree.put(root, new Node());
+        stk.addFirst(root);
         while (true) {
             String numStr = br.readLine();
             if (numStr == null) break;
-            addTree(Integer.parseInt(numStr));
+            int num = Integer.parseInt(numStr);
+            if (num < stk.peek()) {
+                tree.get(stk.peek()).left = num;
+                tree.put(num, new Node());
+                stk.addFirst(num);
+            }
+            else {
+                int parent = stk.pollFirst();
+                while (!stk.isEmpty() && num > stk.peek()) {
+                    parent = stk.pollFirst();
+                }
+                tree.get(parent).right = num;
+                tree.put(num, new Node());
+                // stk.addFirst(parent);
+                stk.addFirst(num);
+            }
         }
-        postOrder(root);
+        postOrder(root, tree);
         System.out.print(sb);
+    }
+ 
+    static void postOrder(int nodeNum, final Map<Integer, Node> tree) {
+        // if (nodeNum == 0) return;
+        Node node = tree.get(nodeNum);
+        if (node.left != 0) postOrder(node.left, tree);
+        if (node.right != 0) postOrder(node.right, tree);
+        sb.append(nodeNum).append("\n");
     }
 }
