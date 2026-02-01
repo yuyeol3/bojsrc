e7f5593f9a8307b8b9f6c3b408efe148bdb8825d
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2026-02-02 04:03:56 +0900

    [Gold IV] Title: 문자열 폭발, Time: 308 ms, Memory: 109800 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,11 +1,9 @@
 import java.io.*;
 import java.util.*;
 
-import javax.swing.plaf.nimbus.State;
 
 class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 
     static class State {
         int idx;
@@ -24,10 +22,8 @@ class Main {
     }
 
     public static void main(String[] args) throws IOException {
-
-
-        String s = br.readLine().stripTrailing();
-        String k = br.readLine().stripTrailing();
+        String s = br.readLine();
+        String k = br.readLine();
 
         boolean[] broken = new boolean[s.length() + 1];
         
@@ -37,35 +33,18 @@ class Main {
         states[statesSize++] = new State(0, 0);
         while (statesSize > 0) {
           
-            // System.out.println("states: " + Arrays.toString(states) + ", top: " + statesSize);
-            // System.out.println("stk: " + Arrays.toString(stk) + ", top: " + stkSize);
-
-
             State state = states[statesSize-1];
             if (state.idx >= s.length()) {
                 statesSize = 0;
                 break;
             }
 
-            // if (state.cnt == k.length()) {
-            //     while (!state.st.isEmpty()) {
-            //         broken[state.st.pop()] = true;
-            //     }
 
-            //     statesSize--;
-            //     if (statesSize >= 1)
-            //         states[statesSize-1].idx = state.idx + 1;
-
-            //     if (statesSize == 0 && state.idx + 1 < s.length()) {
-            //         states[statesSize++] = new State(state.idx + 1, 0);
-            //     }
-            // }
             if (s.charAt(state.idx) == k.charAt(state.cnt)) {
                
                 state.st.push(state.idx);
                 state.idx++;
                 state.cnt++;
-                    // states[statesSize++] = new State(state.idx + 1, state.cnt + 1);
                 
                 if (state.cnt == k.length()) {
                     while (!state.st.isEmpty()) {
@@ -82,16 +61,13 @@ class Main {
                 }
             }
             else {
-                // statesSize = 0;
                 if (state.cnt == 0) {
-                    statesSize = 0; //????
+                    statesSize = 0;
                     states[statesSize++] = new State(state.idx + 1, 0);
-                    // statesSize--;
                 }
                 else states[statesSize++] = new State(state.idx, 0);
             }
         }
-        // System.out.println(Arrays.toString(states));
 
     
         StringBuilder sb = new StringBuilder();
