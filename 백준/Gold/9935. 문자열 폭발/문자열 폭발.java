988524187bd8b82f788bd90d9d08ebe74fdfb4cb
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2026-02-02 23:53:01 +0900

    [Gold IV] Title: 문자열 폭발, Time: 212 ms, Memory: 40000 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -3,41 +3,89 @@ import java.io.*;
 class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    static class State {
+        int idx;
+        int cnt;
+
+        public State(int idx, int cnt) {
+            this.idx = idx;
+            this.cnt = cnt;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         String s = br.readLine();
         String k = br.readLine();
 
-        char[] stk = new char[s.length()+1];
-        int top = 0;
+        boolean[] broken = new boolean[s.length() + 1];
+        
+        int[] stateIdx = new int[s.length()];
+        int[] stateCnt = new int[s.length()];
+        int stateTop = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            stk[top++] = s.charAt(i);
+        int[] st = new int[s.length()];
+        int stTop = 0;
 
-            // 0 1 2 | 3
-            if (stk[top-1] == k.charAt(k.length()-1)) {
-                int base = top - k.length();
-                boolean isBomb = true;
+        stateIdx[stateTop] = stateCnt[stateTop] = 0;
+        stateTop++;
+        while (stateTop > 0) {
+          
+            int sIdx = stateIdx[stateTop-1];
+            int sCnt = stateCnt[stateTop-1];
+            if (sIdx >= s.length()) {
+                stateTop = 0;
+                break;
+            }
 
-                if (base < 0) isBomb = false;
-                for (int j = 0; isBomb && j < k.length(); j++) {
-                    if (stk[base+j] != k.charAt(j))
-                        isBomb = false;
-                }
+            if (s.charAt(sIdx) == k.charAt(sCnt)) {
+               
+                st[stTop++] = sIdx;
+                sIdx = ++(stateIdx[stateTop-1]);
+                sCnt = ++(stateCnt[stateTop-1]);
+                
+                if (sCnt == k.length()) {
+                    for (int i = 0; i < k.length(); i++)
+                        broken[st[--stTop]] = true;
 
-                if (isBomb) {
-                    for (int j = 0; j < k.length(); j++) {
-                        top--;
+                    stateTop--;
+                    if (stateTop >= 1)
+                        stateIdx[stateTop-1] = sIdx;
+
+                    if (stateTop == 0 && sIdx < s.length()) {
+                        stateIdx[stateTop] = sIdx;
+                        stateCnt[stateTop] = 0;
+                        stateTop++;
                     }
                 }
             }
+            else {
+                if (sCnt == 0) {
+                    stateTop = 0;
+                    stTop = 0;
+                    stateIdx[stateTop] = sIdx + 1;
+                    stateCnt[stateTop] = 0;
+                    stateTop++;
+                    // states[statesSize++] = new State(state.idx + 1, 0);
+                }
+                else {
+                    stateIdx[stateTop] = sIdx;
+                    stateCnt[stateTop] = 0;
+                    stateTop++;
+                    // states[statesSize++] = new State(state.idx, 0);
+                }
+            }
         }
+
     
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < top; i++) {
-            sb.append(stk[i]);
+        for (int i = 0; i < s.length(); i++) {
+            if (!broken[i]) 
+                sb.append(s.charAt(i));
         }
         String result = sb.toString();
         System.out.println(result.equals("") ? "FRULA" : result);
     }
+
+
 
 }
