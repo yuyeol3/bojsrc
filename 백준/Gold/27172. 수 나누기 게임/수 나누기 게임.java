c333db23f9de251e2f5f72af5c3e2554d8fd5497
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2026-02-10 14:40:27 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 368 ms, Memory: 32084 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -11,27 +11,19 @@ class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] nums = new int[n];
+        int maximum = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
+            maximum = Math.max(maximum, nums[i]);
             exists[nums[i]] = true;
         }
 
-
         for (int i = 0; i < n; i++) {
-            for (int j = 1; j * j <= nums[i]; j++) {
-                if (nums[i] % j != 0) continue;
-                int r1 = j, r2 = nums[i] / j;
-
-                if (exists[r1]) {
-                    scores[r1]++;
-                    scores[nums[i]]--;
-                }
-
-                if (r1 != r2 && exists[r2]) {
-                    scores[r2]++;
-                    scores[nums[i]]--;
-                }
+            for (int target = 2 * nums[i]; target <= maximum; target += nums[i] ) {
+                if (!exists[target]) continue;
+                scores[target]--;
+                scores[nums[i]]++;
             }
         }
 
@@ -41,31 +33,6 @@ class Main {
         }
         System.out.println(sb);
 
-        /*
-        naive idea
 
-        O(N^2)
-
-        for (int i = 0; i < n-1; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (num[j] % num[i] == 0) {
-                    score[i]++;
-                    score[j]--;
-                }
-                else if (num[i] % num[j] == 0) {
-                    score[i]--;
-                    score[j]++;
-                }
-            }
-        }
-        
-        만약 a % b == 0 이라면
-        b의 약수에 의해서도 a는 나눠진다.
-
-        n * sqrt(n) 으로 모든 수의 모든 약수를 구한다.
-
-        100  -> 1 2 5 10 20 50 100
-
-        */
     }
 }
