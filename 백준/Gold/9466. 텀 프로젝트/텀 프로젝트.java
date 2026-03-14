ef5e614811aaf4c89f8d3b32f0590520302ff3fd
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2026-03-14 16:15:37 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1116 ms, Memory: 304312 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -3,6 +3,25 @@ import java.io.*;
 
 class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] visited;
+    static int[] links;
+    static int groupCount;
+
+    static void dfs(int x) {
+        visited[x] = 1;
+        int next = links[x];
+        if (visited[next] == 0) {
+            dfs(next);
+        }
+        else if (visited[next] == 1) {
+            groupCount++;
+            for (int p = links[next]; p != next; p = links[p])
+                groupCount++;
+            
+        }
+
+        visited[x] = 2;
+    }
 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
@@ -11,31 +30,18 @@ class Main {
             int N = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int[] links = new int[N+1];
-            int[] indegree = new int[N+1];
+            links = new int[N+1];
             for (int i = 1; i <= N; i++) {
                 links[i] = Integer.parseInt(st.nextToken());
-                indegree[links[i]]++;
             }
 
-            Deque<Integer> q = new ArrayDeque<>();
-            int noGroup = 0;
+            groupCount = 0;
+            visited = new int[N+1];
             for (int i = 1; i <= N; i++) {
-                if (indegree[i] == 0) {
-                    q.offerLast(i);
-                    noGroup++;
-                }
+                if (visited[i] == 0) dfs(i);
             }
 
-            while (!q.isEmpty()) {
-                int s = q.pollFirst();
-
-                if (--indegree[links[s]] == 0) {
-                    q.offerLast(links[s]);
-                    noGroup++;
-                }
-            }
-            sb.append(noGroup).append("\n");
+            sb.append(N-groupCount).append("\n");
 
         }
         System.out.print(sb);
